de890b91d0260c7253ccfcfecf1e56ea40121421
2Goddie7 <diego.mullo@epn.edu.ec>
2024-10-15 20:43:35 -0500

    TAREA 2

--- src/Jugador.java
+++ src/Jugador.java
@@ -1,22 +1,36 @@
-public class Libro {
-    String titulo;
-    String autor;
-    int paginaCount;
+//Ejercicio 1 - Tarea 2
+class Jugador {
+    // Atributos de la clase Jugador
+    private String nombre;
+    private String posicion;
+    private int edad;
+    private int numeroCamiseta;
 
-    public Libro(String tituloLibro, String autorLibro, int paginas) {
-        titulo = tituloLibro;
-        autor = autorLibro;
-        paginaCount = paginas;
+    // Constructor de la clase Jugador
+    public Jugador(String nombre, String posicion, int edad, int numeroCamiseta) {
+        this.nombre = nombre;
+        this.posicion = posicion;
+        this.edad = edad;
+        this.numeroCamiseta = numeroCamiseta;
     }
-    public String informarcionLibro() {
-        return "Titulo: " + titulo + "\nAutor: " + autor + "\nPaginas: " + paginaCount;
-    }
-    public static void main(String[] args) {
-        Libro primerLibro = new Libro("\"Matar a un ruiseñor\" (To Kill a Mockingbird)", "Harper Lee", 281);
-        Libro segundoLibro = new Libro("1984", "George Orwell", 328);
 
-        System.out.println(primerLibro.informarcionLibro());
+    // Metodo para mostrar la información del jugador
+    public void mostrarInformacion() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Posición: " + posicion);
+        System.out.println("Edad: " + edad + " años");
+        System.out.println("Número de camiseta: " + numeroCamiseta);
         System.out.println();
-        System.out.println(segundoLibro.informarcionLibro());
+    }
+
+    // Metodo principal para crear objetos y mostrar su información
+    public static void main(String[] args) {
+        Jugador jugador1 = new Jugador("Lionel Messi - La cabra", "Delantero", 36, 10);
+        Jugador jugador2 = new Jugador("Cristiano Ronaldo - El acabado", "Banco", 39, 7);
+        Jugador jugador3 = new Jugador("Neymar - El rey", "Delantero", 32, 10);
+
+        jugador1.mostrarInformacion();
+        jugador2.mostrarInformacion();
+        jugador3.mostrarInformacion();
     }
 }
